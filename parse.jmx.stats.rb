d21a21674c9b98f6fbefc517159cc3adfbac1f00
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2011-11-02 13:57:06 +0200

    ruby script to generate data.js from jmx.stats.csv file

---
+++ parse.jmx.stats.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'csv'
+
+input = CSV.read("jmx.stats.csv", :headers => true)
+output = File.open("data.js", "w")
+
+output.puts("var plotData = new Hash();")
+
+input.headers.each_with_index do |header, index|
+  data = []
+  input.each do |row|
+    data << row[index]
+  end
+  data.map! do |e|
+    begin
+      Integer(e)
+    rescue
+      e
+    end
+  end
+  data.collect!.with_index{ |e, i| [i,e] }
+  output.puts("plotData.set('#{header}', #{data.to_s});")
+end
